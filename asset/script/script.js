cc42c6b804cc8f9c4c288a5ebb5de431ef4db456
TEHSUS <93588863+tchtaibii@users.noreply.github.com>
2022-10-13 21:25:45 +0100

    .

--- asset/script/script.js
+++ asset/script/script.js
@@ -5,3 +5,17 @@ function showbar(){
     else
         m.style.display = "flex";
 }
+function ft_scroll(svg, sc){
+    var img = document.getElementById(svg);
+    var div = document.getElementById(sc);
+    console.log(img);
+    if (div.style.display == 'block')
+        div.style.display = 'none';
+    else
+        div.style.display = 'block';
+    
+    if (img.getAttribute('d') == 'M18 12H6')
+        img.setAttribute('d', 'M12 6v6m0 0v6m0-6h6m-6 0H6');
+    else
+        img.setAttribute('d', 'M18 12H6');
+}
