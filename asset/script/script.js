7f421a5c099619d91357e9ff8e75267ba1d1b147
TEHSUS <93588863+tchtaibii@users.noreply.github.com>
2022-10-13 16:29:35 +0100

    Initial commit

---
+++ asset/script/script.js
@@ -0,0 +1,7 @@
+function showbar(){
+    var m = document.getElementById("navmobile");
+    if (m.style.display == "flex")
+        m.style.display = "none";
+    else
+        m.style.display = "flex";
+}
